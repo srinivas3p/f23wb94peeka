4661b521a6fc8c2ddb7df68c02cb672573e71afe
srinivas3p <87357998+srinivas3p@users.noreply.github.com>
2023-10-29 13:47:50 -0500

    nwclass

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var nwclassRouter = require('./routes/nwclass');
-var boardRouter = require('./routes/board');
+
 var app = express();
 
 // view engine setup
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/nwclass', nwclassRouter);
-app.use('/board', boardRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
