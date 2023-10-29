c363e85e9101569109aabf0a277d4cf5de118fce
srinivas3p <87357998+srinivas3p@users.noreply.github.com>
2023-10-29 00:59:13 -0500

    nwclass

---
+++ routes/nwclass.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/nwclass', function(req, res, next) {
+  res.render('nwclass', { title: 'Search Results NuclearWarheads' });
+});
+
+module.exports = router;
