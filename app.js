caaba018539b9a0c506c89d7767072a582e29742
srinivas3p <87357998+srinivas3p@users.noreply.github.com>
2023-10-30 00:12:01 -0500

    final

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var nwclassRouter = require('./routes/nwclass');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/nwclass', nwclassRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
